99d3d00cbaf05150202b819fb51fc672caf2a827
Joe Sullivan <itsjoesullivan@gmail.com>
2016-05-28 12:56:08 -0400

    Cleaner

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
@@ -6,18 +5,10 @@ import App from './containers/App'
 import configureStore from './store/configureStore'
 import './style.css'
 
-const store = configureStore()
 
 render(
-  <Provider store={store}>
+  <Provider store={configureStore()}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
-
-
-setTimeout(function() {
-  var script = document.createElement('script');
-  script.innerHTML = document.getElementById('ga').innerHTML;
-  document.body.appendChild(script);
-}, 1000);
+);
