c89cdc8995e69e5a3c514f784728597946e563b9
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 19:21:08 -0400

    more building

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 echo "SCRIPT"
+NODE_ENV=production webpack -p --config webpack.production.config.js
+HASH=`cat dist/bundle.js | shasum | awk '{print substr($0,0,5)}'`
+mv dist/bundle.js dist/bundle-$HASH.js
+cp index.html dist/index.html
+html-minifier dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
+mv dist/index2.html dist/index.html
+sed -i -e "s/\/static\/bundle\.js/http\:\/\/dyclrq6t27il\.cloudfront\.net\/bundle-$HASH\.js/g" dist/index.html
+rm dist/index.html-e
+gzip dist/index.html
+mv dist/index.html.gz dist/index.html
