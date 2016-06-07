21bb4cbc329bf977b254a35ae3b2ba4dd98548ee
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 19:28:16 -0400

    list

--- scripts/script.sh
+++ scripts/script.sh
@@ -7,6 +7,8 @@ cp index.html dist/index.html
 ./node_modules/html-minifier/cli.js dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
 mv dist/index2.html dist/index.html
 sed -i -e "s/\/static\/bundle\.js/http\:\/\/dyclrq6t27il\.cloudfront\.net\/bundle-$HASH\.js/g" dist/index.html
+echo "dist"
+ls dist
 rm dist/index.html-e
 gzip dist/index.html
 mv dist/index.html.gz dist/index.html
