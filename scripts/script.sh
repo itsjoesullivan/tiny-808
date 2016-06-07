7220900e4e2d3f8226357a9421594f588dcac4de
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 19:25:03 -0400

    html-minifier as a dep

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,7 +4,7 @@ NODE_ENV=production webpack -p --config webpack.production.config.js
 HASH=`cat dist/bundle.js | shasum | awk '{print substr($0,0,5)}'`
 mv dist/bundle.js dist/bundle-$HASH.js
 cp index.html dist/index.html
-html-minifier dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
+./node_modules/html-minifier/cli.js dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
 mv dist/index2.html dist/index.html
 sed -i -e "s/\/static\/bundle\.js/http\:\/\/dyclrq6t27il\.cloudfront\.net\/bundle-$HASH\.js/g" dist/index.html
 rm dist/index.html-e
