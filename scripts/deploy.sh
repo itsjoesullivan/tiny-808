dfbc104cde655d55296693bf4d97b938e8ad80bc
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 19:55:16 -0400

    test variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "DEPLOY"
+echo $test_variable
 
 s3cmd sync dist/ s3://tiny-808.com
 s3cmd modify s3://tiny-808.com/bundle-$HASH.js --add-header=Cache-Control:max-age=315360000
