941d9e925aafa513c1e2f629d8e4a956aefc9431
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 20:25:55 -0400

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 echo "INSTALL"
 npm install
+
+wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+sudo apt-get update
+
 sudo apt-get install s3cmd=1.6.0
