46452b3983ae882676c119bf034540b5c7a606de
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 21:29:45 -0400

    Revert "rearrange"
    This reverts commit 1c954f0da12ba06a9550ba3d83cfcd55ede01fb3.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 echo "DEPLOY"
+npm install
+NODE_ENV=production ./node_modules/webpack/bin/webpack.js -p --config webpack.production.config.js
 
 ls
 
+HASH=`cat dist/bundle.js | shasum | awk '{print substr($0,0,5)}'`
+mv dist/bundle.js dist/bundle-$HASH.js
+cp index.html dist/index.html
+./node_modules/html-minifier/cli.js dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
+mv dist/index2.html dist/index.html
+sed -i -e "s/\/static\/bundle\.js/http\:\/\/dyclrq6t27il\.cloudfront\.net\/bundle-$HASH\.js/g" dist/index.html
+gzip dist/index.html
+mv dist/index.html.gz dist/index.html
 
 s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key \
   sync dist/ s3://tiny-808.com
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 echo "INSTALL"
-
+npm install
 
 wget http://ufpr.dl.sourceforge.net/project/s3tools/s3cmd/1.6.1/s3cmd-1.6.1.tar.gz
 tar xzf s3cmd-1.6.1.tar.gz
 cd s3cmd-1.6.1
 sudo python setup.py install
-
-npm install
