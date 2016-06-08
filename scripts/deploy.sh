a9a600005326ac8952f31533ee09110116a36758
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 20:13:57 -0400

    move hashing to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 echo "DEPLOY"
+
+HASH=`cat dist/bundle.js | shasum | awk '{print substr($0,0,5)}'`
+mv dist/bundle.js dist/bundle-$HASH.js
+cp index.html dist/index.html
+./node_modules/html-minifier/cli.js dist/index.html > dist/index2.html --collapse-whitespace --minify-css --minify-js --remove-attribute-quotes --use-short-doctype
+mv dist/index2.html dist/index.html
+sed -i -e "s/\/static\/bundle\.js/http\:\/\/dyclrq6t27il\.cloudfront\.net\/bundle-$HASH\.js/g" dist/index.html
+gzip dist/index.html
+mv dist/index.html.gz dist/index.html
+
 echo $test_variable
 echo "HASH"
 echo $HASH
