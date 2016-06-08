e50f5532df7944987d0704c00507921cee962aa8
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 20:01:12 -0400

    use keys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo "DEPLOY"
 echo $test_variable
 
-s3cmd sync dist/ s3://tiny-808.com
-s3cmd modify s3://tiny-808.com/bundle-$HASH.js --add-header=Cache-Control:max-age=315360000
-s3cmd modify s3://tiny-808.com/index.html --add-header=Content-Encoding:gzip
-s3cmd modify s3://tiny-808.com/index.html --add-header=Content-Type:text/html
+s3cmd sync dist/ s3://tiny-808.com --access_key=$s3_access_key --secret_key=$s3_secret_key
+s3cmd modify s3://tiny-808.com/bundle-$HASH.js --add-header=Cache-Control:max-age=315360000 --access_key=$s3_access_key --secret_key=$s3_secret_key
+s3cmd modify s3://tiny-808.com/index.html --add-header=Content-Encoding:gzip --access_key=$s3_access_key --secret_key=$s3_secret_key
+s3cmd modify s3://tiny-808.com/index.html --add-header=Content-Type:text/html --access_key=$s3_access_key --secret_key=$s3_secret_key
