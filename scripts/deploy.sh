519a39d99ab17e8d3e7482a20f7818eaaa563cc8
Joe Sullivan <itsjoesullivan@gmail.com>
2016-06-07 20:10:34 -0400

    hash

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 echo "DEPLOY"
 echo $test_variable
+echo "HASH"
+echo $HASH
 
-s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key sync dist/ s3://tiny-808.com
-s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key modify s3://tiny-808.com/bundle-$HASH.js --add-header=Cache-Control:max-age=315360000
-s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key modify s3://tiny-808.com/index.html --add-header=Content-Encoding:gzip
-s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key modify s3://tiny-808.com/index.html --add-header=Content-Type:text/html
+s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key \
+  sync dist/ s3://tiny-808.com
+s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key \
+  modify s3://tiny-808.com/bundle-$HASH.js --add-header=Cache-Control:max-age=315360000
+s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key \
+  modify s3://tiny-808.com/index.html --add-header=Content-Encoding:gzip
+s3cmd --access_key=$s3_access_key --secret_key=$s3_secret_key \
+  modify s3://tiny-808.com/index.html --add-header=Content-Type:text/html
